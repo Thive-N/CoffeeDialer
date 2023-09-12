28c5706c99fa67e1e381eece0251126772072546
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-09-12 11:49:17 +0100

    Chore: Fixing sqlite3 dependency and cleaning up database.ts

--- src/main/database.ts
+++ src/main/database.ts
@@ -1,10 +1,9 @@
 import * as sqlite3 from 'sqlite3';
 
-const DBSOURCE = 'assets/db.sqlite';
 export default class Database {
 	private handle: sqlite3.Database;
 
-	constructor() {
+	constructor(DBSOURCE: string = 'assets/db.sqlite') {
 		this.handle = new sqlite3.Database(DBSOURCE, () => {});
 	}
 }
--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import Database from './database';
 
 class AppUpdater {
 	constructor() {
@@ -25,12 +26,17 @@ class AppUpdater {
 
 let mainWindow: BrowserWindow | null = null;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const db = new Database();
+
 // // example ipc communication between main and renderer
 ipcMain.on('ipc-example', async (event, arg) => {
 	const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
 	console.log(msgTemplate(arg));
 	event.reply('ipc-example', msgTemplate('pong'));
 });
+
+ipcMain.on('send-query', async () => {});
 
 if (process.env.NODE_ENV === 'production') {
 	const sourceMapSupport = require('source-map-support');
