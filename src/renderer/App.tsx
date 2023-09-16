f1f1d3b00e999f4c5b7553f4d4f08289459fd21e
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-09-16 19:47:39 +0100

    Last steps to start development

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,22 +1,11 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-
-function Hello() {
-	return (
-		<div>
-			<div className="Hello">
-				<img width="200" alt="icon" />
-			</div>
-			<h1>electron-react-typescript</h1>
-		</div>
-	);
-}
+import Home from './components/Home/Home';
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Hello />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</Router>
 	);
---
+++ src/renderer/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+	return (
+		<div>
+			<div className="Hello" />
+			<h1>electron-react-typescript</h1>
+		</div>
+	);
+}
+
+export default Home;
