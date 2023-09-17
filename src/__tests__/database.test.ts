b23429a1ad81a473873be5c16858767d874f0b88
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-09-17 12:39:10 +0100

    More tests

--- src/__tests__/database.test.ts
+++ src/__tests__/database.test.ts
@@ -16,9 +16,38 @@ describe('database.addBeans', () => {
 			name: 'test beans',
 			roast: 'light',
 			company: 'test company',
-			roast_date: new Date(),
+			roast_date: new Date(Date.now()),
 		};
 		const result = await db.addBeans(beans);
+		expect(result).toBe(true);
+	});
+});
+
+describe('database.addBrew', () => {
+	test('adding', async () => {
+		const brew: models.Brew = {
+			beans_id: 1,
+			brew_date: new Date(Date.now()),
+			brew_time: 30,
+			grind_in: 30,
+			coffee_out: 30,
+			grind_size: 30,
+		};
+		const result = await db.addBrew(brew);
+		expect(result).toBe(true);
+	});
+});
+
+describe('database.addRating', () => {
+	test('adding', async () => {
+		const rating: models.Rating = {
+			brew_id: 1,
+			rating: 3,
+			notes: 'test notes',
+			bitterness: 3,
+			acidity: 3,
+		};
+		const result = await db.addRating(rating);
 		expect(result).toBe(true);
 	});
 });
