9fc99a144c7db7a1f2ed90eb79dbab00e83949ac
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-09-23 18:03:48 +0100

    Changing file locations

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Home from './pages/Home/Home';
 
 export default function App() {
 	return (
